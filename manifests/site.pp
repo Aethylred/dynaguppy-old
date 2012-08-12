9b4447aeeee8937dad6e8cb26b9415fdb175efd1
Aaron Hicks <Aethylred@gmail.com>
2012-08-13 10:32:44 +1200

    stupid typo

--- manifests/site.pp
+++ manifests/site.pp
@@ -3,7 +3,7 @@
 # These are global variables required by dynaguppy
 
 # These subdirectories are required for dynaguppy
-include "./dynaguppty_conf.pp"
+include "./dynaguppy_conf.pp"
 include "./puppet/*"
 include "./git/*"
 
