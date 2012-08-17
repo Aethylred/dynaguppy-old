8ba062eb5eebf8b556cab4783687c81815fc2f74
Aaron Hicks (Ivy) <Aethylred@gmail.com>
2012-08-17 22:19:52 +1200

    import nodes, include class

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,9 +1,7 @@
 # The primary site manifest
 
-# These are global variables required by dynaguppy
-
 # These subdirectories are required for dynaguppy
-include "./puppet/*"
-include "./git/*"
+import puppet/*
+import git/*
 
 # Add other manifest subdirectories here
