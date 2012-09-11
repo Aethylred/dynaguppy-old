2b5c076e607ed55e0838b3319ff4636f0df55f49
Aaron Hicks <Aethylred@gmail.com>
2012-09-11 13:34:40 +1200

    change to using PuppetLabs repo, and adding a SSH key to the root user.

--- manifests/puppet/dynaguppy-puppetmaster.pp
+++ manifests/puppet/dynaguppy-puppetmaster.pp
@@ -12,7 +12,33 @@ node 'puppet.local' {
 
 	class {'puppet':
 		pluginsync => true,
+		puppetlabs_repo => true,
+	}
+
+	# For Dynaguppy to work, the puppet user needs to be able to log
+	# in remotely with ssh
+	user{'puppet':
+		ensure	=> present,
+		shell 	=> '/bin/bash',
+		require	=> Class['puppet'],
 	}
 	
 	class {'git': }
+
+	# For managing rsa key pair distribution use...
+	include sshkey
+	include sshkey::keymaster
+
+	# Note keys _must_ be declared in the same
+	# environment as the keymaster/puppetmaster, not in the environment
+	# of the node that will use them
+
+	# declare the key for the puppet user
+
+	sshkey::key{'puppet':
+		user 	=> $puppet::params::user,
+		home	=> $puppet::params::user_home,
+	}
+
+
 }
