6e21b51bf1d43254fa64cae895f8560430bf2786
Aaron Hicks <Aethylred@gmail.com>
2012-09-13 11:59:05 +1200

    the puppet user should be declared in the puppet module, and the puppet module can now set the puppet user's shell

--- manifests/puppet/dynaguppy-puppetmaster.pp
+++ manifests/puppet/dynaguppy-puppetmaster.pp
@@ -11,19 +11,12 @@ node 'puppet.local' {
 	}
 
 	class {'puppet':
-		pluginsync => true,
-		storeconfig => true,
+		pluginsync 			=> true,
+		storeconfig 		=> true,
 		puppetlabs_repo => true,
+		user_shell			=> '/bin/bash',
 	}
 
-	# For Dynaguppy to work, the puppet user needs to be able to log
-	# in remotely with ssh
-	user{'puppet':
-		ensure	=> present,
-		shell 	=> '/bin/bash',
-		require	=> Class['puppet'],
-	}
-	
 	class {'git': }
 
 	# For managing rsa key pair distribution use...
@@ -34,11 +27,11 @@ node 'puppet.local' {
 	# environment as the keymaster/puppetmaster, not in the environment
 	# of the node that will use them
 
-	# declare the key for the puppet user
-
+	# declare the key for the puppet user, which is declared in the puppet module
 	sshkey::key{'puppet':
 		user 	=> $puppet::params::user,
 		home	=> $puppet::params::user_home,
+		require => User['puppet'],
 	}
 
 
