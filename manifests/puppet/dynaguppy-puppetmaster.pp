75da6fa73b18997d3fd5f73eecbb3fdfde727f29
Aaron Hicks (Shiny-XPS) <hicksa@landcareresearch.co.nz>
2013-01-24 15:05:09 +1300

    installing the build packages conflicts with calling the gcc class so the puppetlabs/gcc module needs to be installed  and the gcc class called before puppet-librarian is installed.

--- manifests/puppet/dynaguppy-puppetmaster.pp
+++ manifests/puppet/dynaguppy-puppetmaster.pp
@@ -4,10 +4,25 @@
 
 node 'puppet.local' {
 
-	package{['ruby-dev','build-essential']:
-		ensure		=> installed,
+# These conflict with the installation of the puppetlabs/gcc module with puppet-librarian
+	# package{['ruby-dev','build-essential']:
+	# 	ensure		=> installed,
+	# }
+
+	file{'/etc/puppet/modules':
+		ensure	=> directory,
 	}
 
+	exec{'dynaguppy_bootstrap_gcc':
+		command	=> 'puppet module install puppetlabs/gcc',
+		cwd			=> '/etc/puppet/modules',
+		require	=> File['/etc/puppet/modules'],
+		creates	=> '/etc/puppet/modules/gcc',
+	}
+
+	class {'gcc':
+		require => Exec['dynaguppy_bootstrap_gcc'],
+	}
 
 # maestrodev has some patches we need!
 
@@ -28,7 +43,6 @@ node 'puppet.local' {
 	}
 
 	class {'apache': }
-	class {'gcc': }
 	class {'ruby': }
 	class {'passenger':	
 	require	=> Class['apache','gcc','ruby'],
