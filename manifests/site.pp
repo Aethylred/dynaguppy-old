9f208b3a296685a2eae38c30c67fbcbc0a48abd9
Aaron Hicks (Shiny-XPS) <hicksa@landcareresearch.co.nz>
2013-02-13 13:53:41 +1300

    The FQDN for the git server and the puppetmaster server will be scattered through the dynaguppy configuration, will be simplest to configure them as a global variable at the moment. Will have to look at using either hiera or PuppetDB to store it at a later stage.

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,5 +1,10 @@
 # The primary site manifest
 
+# Set the names for the key servers in the dynaguppy setup
+
+$puppetmaster_fqdn = 'puppet.local'
+$git_fqdn	= 'git.local'
+
 # These subdirectories are required for dynaguppy
 import "./puppet/*"
 import "./git/*"
