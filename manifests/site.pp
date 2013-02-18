72bce91b10a3b1138bef46ac8f9003210f9a6cf7
Aaron Hicks (Shiny-XPS) <hicksa@landcareresearch.co.nz>
2013-02-18 23:57:18 +1300

    Preconfiguring firewall module

--- manifests/site.pp
+++ manifests/site.pp
@@ -5,6 +5,37 @@
 $puppetmaster_fqdn = 'puppet.local'
 $git_fqdn	= 'git.local'
 
+# Configure the firewall defaults as per https://github.com/puppetlabs/puppetlabs-firewall
+# Always persist firewall rules
+exec { 'persist-firewall':
+  command     => $operatingsystem ? {
+    'debian'          => '/sbin/iptables-save > /etc/iptables/rules.v4',
+    /(RedHat|CentOS)/ => '/sbin/iptables-save > /etc/sysconfig/iptables',
+  },
+  refreshonly => true,
+}
+
+# These defaults ensure that the persistence command is executed after 
+# every change to the firewall, and that pre & post classes are run in the
+# right order to avoid potentially locking you out of your box during the
+# first puppet run.
+Firewall {
+  notify  => Exec['persist-firewall'],
+  before  => Class['my_fw::post'],
+  require => Class['my_fw::pre'],
+}
+Firewallchain {
+  notify  => Exec['persist-firewall'],
+}
+
+# Purge unmanaged firewall resources
+#
+# This will clear any existing rules, and make sure that only rules
+# defined in puppet exist on the machine
+resources { "firewall":
+  purge => true
+}
+
 # These subdirectories are required for dynaguppy
 import "./puppet/*"
 import "./git/*"
