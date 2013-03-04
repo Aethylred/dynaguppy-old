9c8ed614192b62d12e19256f5c44c4e9bf0313a4
Aaron Hicks (Shiny-XPS) <hicksa@landcareresearch.co.nz>
2013-03-05 09:54:47 +1300

    Updated firewall implementation as per advice given: https://github.com/puppetlabs/puppetlabs-firewall/issues/139

--- modules/dynaguppy/manifests/firewall/persist.pp
+++ modules/dynaguppy/manifests/firewall/persist.pp
@@ -5,26 +5,14 @@
 # https://github.com/puppetlabs/puppetlabs-firewall/blob/master/README.markdown
 
 class dynaguppy::firewall::persist{
-  # Always persist firewall rules
-  exec { 'persist-firewall':
-    command     => $operatingsystem ? {
-      /(Debian|Ubuntu)/ => '/sbin/iptables-save > /etc/iptables/rules.v4',
-      /(RedHat|CentOS)/ => '/sbin/iptables-save > /etc/sysconfig/iptables',
-    },
-    refreshonly => true,
-  }
 
   # These defaults ensure that the persistence command is executed after
   # every change to the firewall, and that pre & post classes are run in the
   # right order to avoid potentially locking you out of your box during the
   # first puppet run.
   Firewall {
-    notify  => Exec['persist-firewall'],
     before  => Class['dynaguppy::firewall::post'],
     require => Class['dynaguppy::firewall::pre'],
-  }
-  Firewallchain {
-    notify  => Exec['persist-firewall'],
   }
 
   # Purge unmanaged firewall resources
